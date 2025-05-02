1f25cc4b6da1ccc58820364aee3c454b9666bff2
razonno <nrazonno@gmail.com>
2025-05-01 20:52:43 -0400

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,6 @@
+temperature = int(input("Enter the current temperature in °C: "))
+
+if temperature >= 20:
+    print("It's warm enough. You can wear light clothes.")
+else:
+    print("It's too cold for light clothes. Better wear something warm.")
